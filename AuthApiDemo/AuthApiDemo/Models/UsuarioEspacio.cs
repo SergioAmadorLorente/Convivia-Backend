48eb93f6f2470861e63bc366b8fb068d2520f7cc
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-09-25 08:56:42 +0200

    [Backend] 25/09/2025 UsuarioEspacio - Constructos acabats, comentaris afegits

--- AuthApiDemo/AuthApiDemo/Models/UsuarioEspacio.cs
+++ AuthApiDemo/AuthApiDemo/Models/UsuarioEspacio.cs
@@ -9,6 +9,7 @@
         public bool ausente { get; set; }
 
         public int karma { get; set; }
+        public string rol { get; set; }
 
         public Espacio espacio { get; set; }
 
@@ -18,11 +19,36 @@
         public Permiso permiso { get; set; }
         public List<Factura> Facturas { get; set; } = new List<Factura>(); // ?
 
+        // Constructor por defecto vacio para pruebas
         public UsuarioEspacio()
         {
         }
 
-        public UsuarioEspacio(Usuario usuario, Espacio espacio, Permiso permiso, bool ausente = false,  int karma = 0)
+        // Constructor que asigna el permiso según el rol proporcionado
+        public UsuarioEspacio(Usuario usuario, Espacio espacio, Permiso permiso, bool ausente = false, int karma = 0, string rol)
+        {
+            Id_UsuarioEspacio = Guid.NewGuid().ToString();
+            this.usuario = usuario;
+            this.espacio = espacio;
+            this.ausente = ausente;
+            this.karma = karma;
+
+            if (rol == "admin")
+            {
+                this.permiso = Permiso.Admin;
+            }
+            else if (rol == "usuario")
+            {
+                this.permiso = Permiso.Usuario;
+            }
+            else
+            {
+                this.permiso = Permiso.Huesped;
+            }
+        }
+
+        // Constructor que recibe un objeto Permiso directamente
+        public UsuarioEspacio(Usuario usuario, Espacio espacio, Permiso permiso, bool ausente = false, int karma = 0)
         {
             Id_UsuarioEspacio = Guid.NewGuid().ToString();
             this.usuario = usuario;
@@ -32,16 +58,22 @@
             this.permiso = permiso;
         }
 
+        // Métodos
+
+        // Cambia el permiso del usuario en el espacio
         public void cambiarPermiso(Permiso permiso)
         {
             this.permiso = permiso;
         }
 
+        // Marca al usuario como ausente
         public void salirEspacio()
         {
             espacio.Usuarios.Remove(this.usuario);
             this.usuario.UsuariosEspacios.Remove(this);
         }
+
+        // Marca al usuario como presente
         public void unirseEspacio(Espacio espacio)
         {
             if (!espacio.Usuarios.Contains(this.usuario))
@@ -53,22 +85,27 @@
                 this.usuario.UsuariosEspacios.Add(this);
             }
         }
+
+        // Suma puntos de karma al usuario
         public void sumKarma(int puntos)
         {
             this.karma += puntos;
         }
 
+        // Suma puntos de karma y marca la tarea como completada
         public void sumKarmaAndPassTask(Tarea tarea)
         {
             tarea.cambiarEstado(true);
             karma += tarea.karma;
         }
 
+        // Resta puntos de karma al usuario
         public void creearTarea(Tarea tarea)
         {
             this.TareasAsignadas.Add(tarea);
             tarea.Usuarios.Add(this);
         }
+
         public void crearTareaDePlantilla()
         {
             // TODO
@@ -77,12 +114,17 @@
         public void crearPlantilla()
         {
             // TODO
+        
         }
+
+        // Asigna una tarea al usuario
         public void guardarTarea(Tarea tarea)
         {
             tarea.agregarUsuarios(new List<UsuarioEspacio> { this });
             TareasAsignadas.Add(tarea);
         }
+
+        // Elimina una tarea asignada al usuarios
         public void reservarSala(Sala sala, DateTime fechaInicio, DateTime fechaFin)
         {
             if (sala.crearReserva(fechaInicio, fechaFin, this.usuario))
@@ -95,6 +137,7 @@
             }
         }
 
+        // Elimina una reserva realizada por el usuario
         public void cancelarReserva(Sala sala, Reserva r)
         {
             sala.eliminarReserva(r.Id_Reserva);
