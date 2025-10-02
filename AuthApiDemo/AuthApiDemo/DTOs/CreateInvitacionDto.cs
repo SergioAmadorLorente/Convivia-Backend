953cbc8d2029377288395dd63c1981ede0cb788f
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-10-02 10:03:57 +0200

    [BACKEND] 02/10/2025 DTOs - Creacion de la carpeta DTOs, creacion de CreateInviacionDto

---
+++ AuthApiDemo/AuthApiDemo/DTOs/CreateInvitacionDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthApiDemo.DTOs
+{
+    public class CreateInvitacionDto
+    {
+        [Required]
+        public string UsuarioSolicitanteId { get; set; } = default;
+
+        [Required]
+        public string UsuariosInvitadoId { get; set; } = default;
+
+        [Required]
+        public string EspacioId { get; set; } = default;
+        
+        // mensaje que envia el solicitante
+        public string? Mensaje {  get; set; }
+    }
+}
