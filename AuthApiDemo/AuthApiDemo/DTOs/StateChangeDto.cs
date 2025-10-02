58c1f937f5ffc2b35abf12f77af36cb896fe417f
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-10-02 10:17:54 +0200

    [BACKEND] 02/10/2025 DTOs StateChangeDto.cs - Creacion del DTO de cambio de estado

---
+++ AuthApiDemo/AuthApiDemo/DTOs/StateChangeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthApiDemo.DTOs
+{
+    public class StateChangeDto
+    {
+        public string NuevoEstado { get; set; } = default!;
+        public string? AccionanteUsuarioId { get; set; }
+    }
+}
