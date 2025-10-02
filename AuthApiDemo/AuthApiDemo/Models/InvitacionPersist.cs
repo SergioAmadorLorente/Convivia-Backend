411cbb40e1c7e64ab5ec8d61967a4254faf1e302
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-10-02 11:15:10 +0200

    [BACKEND] 02/10/2025 Modelo InvitacionPersist - Creacion del modelos InvitacionPersist.cs

---
+++ AuthApiDemo/AuthApiDemo/Models/InvitacionPersist.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthApiDemo.Models
+{
+    public class InvitacionPersist
+    {
+        public string Id { get; set; } = default!;
+        public string UsuarioSolicitanteId { get; set; } = default!;
+        public string UsuarioInvitadoId { get; set; } = default!;
+        public string EspacioId { get; set; } = default!;
+        public string Mensaje { get; set; } = default!;
+        public DateTime Fecha { get; set; }
+        public string Estado { get; set; } = default!;
+    }
+}
