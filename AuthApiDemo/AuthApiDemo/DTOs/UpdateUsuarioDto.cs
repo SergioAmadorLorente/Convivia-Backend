d08b179407f1cc49f4c09c396a8ea802b4d2cf89
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-10-17 10:33:33 +0200

    [Backend] 17/10/2025 UpdateUsuarioDto - UpdateUsuarioDto creado y terminado.

---
+++ AuthApiDemo/AuthApiDemo/DTOs/UpdateUsuarioDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthApiDemo.DTOs
+{
+    public class UpdateUsuarioDto
+    {
+        public string? Nombre { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Telefono { get; set; }
+    }
+}
