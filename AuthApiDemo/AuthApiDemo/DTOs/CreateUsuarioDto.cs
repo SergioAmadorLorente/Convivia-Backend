b40951a929ae8a294dd54d6c4d3bbb26d54e1cd1
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-10-17 10:30:38 +0200

    [Backend] CreateUsuarioDto - CreateUsusarioDto creado y terminado.

---
+++ AuthApiDemo/AuthApiDemo/DTOs/CreateUsuarioDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthApiDemo.DTOs
+{
+    public class CreateUsuarioDto
+    {
+        public string Nombre { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string? Telefono { get; set; }
+        public string Premium { get; set; } = "false";
+    }
+}
