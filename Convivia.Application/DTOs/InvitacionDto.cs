925949726943e6b20c84766300946359b4764863
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-11-17 11:17:20 +0100

    [17/11/2025] InvitacionDto.cs Creado

---
+++ Convivia.Application/DTOs/InvitacionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convivia.Application.DTOs
+{
+    internal class InvitacionDto
+    {
+        public string Id { get; set; }
+        public string UsuarioSolicitanteId { get; set; }
+        public string UsuarioInvitadoID { get; set; }
+        public string EspacioId { get; set; }
+        public string Mensaje { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
