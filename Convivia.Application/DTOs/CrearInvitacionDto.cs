4f420e513284dab4525c0139a77ed5f3fbf350c2
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-11-17 11:08:16 +0100

    [17/11/2025] CreateInvitacionDto.cs creado.

---
+++ Convivia.Application/DTOs/CrearInvitacionDto.cs
@@ -0,0 +1,17 @@
+﻿using Google.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convivia.Application.DTOs
+{
+    internal class CrearInvitacionDto
+    {
+        public string UsuarioSolcitanteId { get; set; }
+        public string UsuarioInvitadoId { get; set; }
+        public string EspacioId { get; set; }
+        public string? Mensaje { get; set; }
+    }
+}
--- Convivia.Domain/Models/Invitacion.cs
+++ Convivia.Domain/Models/Invitacion.cs
@@ -11,7 +11,7 @@
 
         public Espacio Espacio { get; set; }
 
-        public string Mensaje { get; set; }
+        public string? Mensaje { get; set; }
 
         public DateTime Fecha { get; set; } = DateTime.UtcNow;
 
