e8a5029cf5f66950371c49d1adace6fc7a1613d9
YoussefGhaddari <yousseh.ghaddari.ext@inetum.com>
2025-11-21 12:13:09 +0100

    Creation of serviceCollectionExtensions in infrastructure and application

---
+++ Convivia.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Convivia.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Convivia.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Registrar servicios de aplicación
+            // Si tienes interfaz IInvitacionService: services.AddScoped<IInvitacionService, InvitacionService>();
+            services.AddScoped<InvitacionService>();
+
+            // Registrar mappers, validators, MediatR, etc. si procede
+            // services.AddAutoMapper(typeof(YourProfile).Assembly);
+
+            return services;
+        }
+    }
+}
---
+++ Convivia.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Convivia.Shared.Repositories;
+using Convivia.Shared.Services;
+using Convivia.Infrastructure.Repositories;
+using Convivia.Infrastructure.Services;
+
+namespace Convivia.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Repositorios concretos
+            services.AddScoped<IInvitacionRepository, InvitacionRepository>();
+
+            // Servicios de infraestructura
+            services.AddScoped<IFirebaseService, FirebaseService>();
+
+
+            return services;
+        }
+    }
+}
