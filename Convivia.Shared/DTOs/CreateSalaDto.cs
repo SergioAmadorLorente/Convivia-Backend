2cc1be76a7035811035e7349fb9985cbeb7d73cb
MarcAInetum <marc.arbiol.ext@inetum.com>
2025-11-25 10:31:02 +0100

    Change the site of Dto and create the interface of SalaRepository

---
+++ Convivia.Shared/DTOs/CreateSalaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convivia.Shared.DTOs
+{
+    public class CreateSalaDto
+    {
+        public string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string IdEspacio { get; set; }
+    }
+}
---
+++ Convivia.Shared/Repositories/ISalaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Convivia.Shared.DTOs;
+
+namespace Convivia.Shared.Repositories
+{
+    public interface ISalaRepository
+    {
+        Task<SalaDto?> GetByIdAsync(string id, CancellationToken ct = default);
+        Task<IEnumerable<SalaDto>> GetByEspacioIdAsync(string espacioId, CancellationToken ct = default);
+        Task<string> AddAsync(SalaDto sala, CancellationToken ct = default);
+        Task UpdateAsync(string id, SalaDto sala, CancellationToken ct = default);
+        Task DeleteAsync(string id, CancellationToken ct = default);
+    }
+}
