aabc5753cd27b347eee593750b8375d18a0ff7a5
EnricFerrando <enricnavarro00@gmail.com>
2025-12-04 10:14:51 +0100

    enitity and model fixed

--- Convivia.Domain/Entities/UsuarioEspacio.cs
+++ Convivia.Domain/Entities/UsuarioEspacio.cs
@@ -5,55 +5,24 @@ namespace Convivia.Domain.Entities
 {
 
 
-    [FirestoreData]
     public class UsuarioEspacio
     {
-        [FirestoreProperty]
         public string Id_UsuarioEspacio { get; set; } = Guid.NewGuid().ToString();
 
-        [FirestoreProperty]
         public bool Ausente { get; set; }
 
-        [FirestoreProperty]
         public int Karma { get; set; }
 
-        [FirestoreProperty]
         public string Rol { get; set; }
 
-        [FirestoreProperty]
-        public DocumentReference EspacioRef { get; set; }
-
-        [JsonIgnore]
         public Espacio Espacio { get; set; }
 
-
-        [FirestoreProperty]
-        public DocumentReference UsuarioRef { get; set; }
-
-
-        [JsonIgnore]
         public Usuario Usuario { get; set; }
 
-
-        [FirestoreProperty("tareas")]
-        public List<DocumentReference> tareasRefs { get; set; } = new();
-
-        [JsonIgnore]
         public List<Tarea> tareas { get; set; } = new();
 
-
-        [FirestoreProperty]
-        public DocumentReference PermisoRef { get; set; }
-
-
-        [JsonIgnore]
         public Permiso Permiso { get; set; }
 
-
-        [FirestoreProperty]
-        public List<DocumentReference> FacturasRefs { get; set; } = new();
-
-        [JsonIgnore]
         public List<Factura> Facturas { get; set; } = new();
 
 
@@ -84,25 +53,6 @@ namespace Convivia.Domain.Entities
             }
         }
 
-        // JSON
-
-        public UsuarioEspacioResponse ToResponse()
-        {
-            return new UsuarioEspacioResponse
-            {
-                Id_UsuarioEspacio = this.Id_UsuarioEspacio,
-                Ausente = this.Ausente,
-                Karma = this.Karma,
-                Rol = this.Rol,
-                UsuarioId = UsuarioRef?.Id,
-                EspacioId = EspacioRef?.Id,
-                PermisoId = PermisoRef?.Id,
-                tareas = tareasRefs?.Select(r => r.Id).ToList() ?? new(),
-                Facturas = FacturasRefs?.Select(r => r.Id).ToList() ?? new()
-            };
-        }
-
-
         // Constructor que recibe un objeto Permiso directamente
         public UsuarioEspacio(Usuario usuario, Espacio espacio, Permiso permiso, bool ausente = false, int karma = 0)
         {
@@ -113,7 +63,5 @@ namespace Convivia.Domain.Entities
             this.Karma = karma;
             this.Permiso = permiso;
         }
-
-        
     }
 }
---
+++ Convivia.Infrastructure/Models/FireStoreUsuarioEspacio.cs
@@ -0,0 +1,39 @@
+﻿using Convivia.Domain.Entities;
+using Google.Cloud.Firestore;
+using System.Text.Json.Serialization;
+namespace Convivia.Domain.Entities
+{
+
+
+    [FirestoreData]
+    public class FireStoreUsuarioEspacio
+    {
+        [FirestoreDocumentId]
+        public string Id_UsuarioEspacio { get; set; } = Guid.NewGuid().ToString();
+
+        [FirestoreProperty("Ausente")]
+        public bool Ausente { get; set; }
+
+        [FirestoreProperty("Karma")]
+        public int Karma { get; set; }
+
+        [FirestoreProperty("Rol")]
+        public string Rol { get; set; }
+
+        [FirestoreProperty("EspacioId")]
+        public string EspacioId { get; set; }
+
+        [FirestoreProperty("UsuarioId")]
+        public string Usuarioíd { get; set; }
+
+        [FirestoreProperty("tareasId")]
+        public List<string> tareasId { get; set; } = new();
+   
+        [FirestoreProperty("PermisoId")]
+        public string PermisoId { get; set; }
+
+        [FirestoreProperty("FacturasId")]
+        public List<string> FacturasId { get; set; } = new();
+
+    }
+}
