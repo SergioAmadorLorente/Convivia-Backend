8b3a97001954ace90c17d4396a938331b1112ada
MarcAInetum <marc.arbiol.ext@inetum.com>
2025-12-09 11:54:45 +0100

    Add the Reserva entity to the MapsterConfig

--- Convivia.Application/Mappers/MapsterBootstrap.cs
+++ Convivia.Application/Mappers/MapsterBootstrap.cs
@@ -34,6 +34,8 @@ namespace Convivia.Application.Mappers
             // PlantillaTarea mappings (DTO <-> Domain)
             Config.MapsterConfig.RegisterPair<PlantillaTarea, PlantillaTareaDto, CreatePlantillaTareaDto, UpdatePlantillaTareaDto>(config);
 
+            Config.MapsterConfig.RegisterPair<Reserva, ReservaDto, CreateReservaDto, UpdateReservaDto>(config);
+
             // Scan infrastructure assembly for IRegister implementations (Domain <-> Persistence)
             try
             {
