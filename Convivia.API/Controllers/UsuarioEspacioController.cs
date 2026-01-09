231e76f315c713ca14809d4cee5ad57e052a275d
MarcSastreCabre <msastrecabre@gmail.com>
2026-01-09 10:35:04 +0100

    add tryCatch in usuario and usuarioEspacio controllers

--- Convivia.API/Controllers/UsuarioEspacioController.cs
+++ Convivia.API/Controllers/UsuarioEspacioController.cs
@@ -108,9 +108,15 @@ namespace Convivia.Api.Controllers
         public async Task<IActionResult> Delete(string id, CancellationToken ct)
         {
             if (string.IsNullOrWhiteSpace(id)) return BadRequest();
-
-            var resultat = await _service.EliminarUsuarioEspacioAsync(id, ct);
-            return resultat ? NoContent() : NotFound();
+            try
+            {
+                var resultat = await _service.EliminarUsuarioEspacioAsync(id, ct);
+                return resultat ? NoContent() : NotFound();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new { message = ex.Message });
+            }
         }
     }
 }
