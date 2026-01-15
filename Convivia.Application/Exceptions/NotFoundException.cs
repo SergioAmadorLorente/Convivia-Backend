aecc72c405782978cd1de76bf9a0dac860d5e2d4
yousseh.ghaddari.ext <yousseh.ghaddari.ext@ITMES5CG1065M63>
2026-01-15 10:10:29 +0100

    Add NotFoundException for missing domain entities

---
+++ Convivia.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace Convivia.Application.Exceptions
+{
+    public class NotFoundException : AppException
+    {
+        public string Entity { get; }
+        public object? Key { get; }
+
+        public NotFoundException(string entity, object? key = null, string? message = null)
+            : base(message ?? $"{entity} not found")
+        {
+            Entity = entity;
+            Key = key;
+        }
+    }
+}
