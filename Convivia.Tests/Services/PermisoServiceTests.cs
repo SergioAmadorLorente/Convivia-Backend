4c42cc57b0fc4ebbc2d3ee6b0f4d28b80091aa2f
EnricFerrando <enricnavarro00@gmail.com>
2026-01-20 12:22:15 +0100

    inicial commit

---
+++ Convivia.Tests/Services/PermisoServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convivia.Tests.Services
+{
+    internal class PermisoServiceTests
+    {
+    }
+}
